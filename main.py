c9da59393e53898005573c84de488f0c6b21c781
OleksandrHub <sasha.klishch1@gmail.com>
2025-02-06 11:52:13 +0200

    Add password generation function and example usage

---
+++ main.py
@@ -0,0 +1,28 @@
+import re
+import string
+import secrets
+def generate_password(length, nums, special_chars, uppercase, lowercase):
+    #Define the possible characters for the password
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
+    #Combine all characters
+    all_characters = letters + digits + symbols
+    while True:
+        password = ""
+        #Generate password
+        for _ in range(length):
+            password += secrets.choice(all_characters)
+        constraints = [
+            (nums, r'\d'), 
+            (lowercase, r'[a-z]'),
+            (uppercase, r'[A-Z]'),
+            (special_chars, r'') 
+        ]
+    return password
+
+# new_password = generate_password(8)
+# print(new_password)
+pattern = '\.'
+quote = 'Not all those who wander are lost.'
+print(re.findall(pattern, quote))
